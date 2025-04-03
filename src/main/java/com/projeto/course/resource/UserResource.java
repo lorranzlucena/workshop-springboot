cd522f5ec0dde75f9a7bb6432d28fa58f889d2a6
lorranzlucena <lorranz.lucena.sousa@gmail.com>
2025-04-03 16:21:36 -0300

    service layer, component registration

--- src/main/java/com/projeto/course/resource/UserResource.java
+++ src/main/java/com/projeto/course/resource/UserResource.java
@@ -1,26 +1,46 @@
 package com.projeto.course.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.course.entities.User;
+import com.projeto.course.services.UserService;
 
 /**
  * CLasse resposavel por reucrsos webs
  */
 
 @RestController // Indica que esta classe é um controlador REST
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users/")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
+	
 	// usada para mapear requisições HTTP do tipo GET 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		
-		User u = new User(1L, "Maria", "mari@gmail.com","999999","123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+
 	}
+	
+	/**
+	 * (@PathVariable : ta dizendo que o Long Id é o valor repassado pela quesição do HTTML
+	 */
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }
---
+++ src/main/java/com/projeto/course/services/UserService.java
@@ -0,0 +1,36 @@
+package com.projeto.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.course.entities.User;
+import com.projeto.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	// Operação na camada de serviço
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long Id) {
+		/**
+		 *  representa um valor que pode ou não estar presente. Ele ajuda a evitar NullPointerException 
+		 *  e melhora a legibilidade do código ao lidar com valores opcionais.
+		 */
+		Optional<User> obj =  repository.findById(Id);
+		return obj.get();
+		// se colocar assim: 
+		//User user = userRepository.findById(id); 
+		// Se findById(id) não encontrar o usuário, user será null e o programa quebrará ao chamar getNome().
+	}
+	
+	
+}
